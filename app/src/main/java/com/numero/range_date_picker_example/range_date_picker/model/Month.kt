d493ae0cc8c87a80ab7bbeb0870becc24e6bd694
“NUmeroAndDev” <numero.dev.and@gmail.com>
2018-05-24 14:03:46 +0900

    Modelの追加

---
+++ app/src/main/java/com/numero/range_date_picker_example/range_date_picker/model/Month.kt
@@ -0,0 +1,9 @@
+package com.numero.range_date_picker_example.range_date_picker.model
+
+import java.util.*
+
+data class Month(
+        val month: Int,
+        val year: Int,
+        val date: Date,
+        var label: String)
---
+++ app/src/main/java/com/numero/range_date_picker_example/range_date_picker/model/MonthCell.kt
@@ -0,0 +1,13 @@
+package com.numero.range_date_picker_example.range_date_picker.model
+
+import java.util.*
+
+data class MonthCell(
+        val date: Date,
+        val isCurrentMonth: Boolean,
+        val isSelectable: Boolean,
+        var isSelected: Boolean,
+        val isToday: Boolean,
+        var isHighlighted: Boolean,
+        val value: Int,
+        var rangeState: RangeState)
