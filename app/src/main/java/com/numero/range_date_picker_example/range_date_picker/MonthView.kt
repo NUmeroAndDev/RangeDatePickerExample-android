246aea458558ec8f461c150bb7139f5dee1a6429
“NUmeroAndDev” <numero.dev.and@gmail.com>
2018-05-24 14:01:47 +0900

    MonthViewの追加

---
+++ app/src/main/java/com/numero/range_date_picker_example/range_date_picker/MonthView.kt
@@ -0,0 +1,16 @@
+package com.numero.range_date_picker_example.range_date_picker
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.numero.range_date_picker_example.R
+
+class MonthView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(context, R.layout.view_month, this)
+
+    }
+
+}
